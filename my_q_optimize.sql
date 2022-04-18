7d8f29ceec1d600773bf0183a8820b52078978b3
Funix565 <denkin.dmitriy@gmail.com>
2022-04-18 22:35:40 +0300

    Style and indentation

--- my_q_optimize.sql
+++ my_q_optimize.sql
@@ -17,61 +17,56 @@
 -- Alternatives that will come in handy here are aggregation functions like MIN or MAX.
 -- that the biggest table is placed last in the join.
 
--- select tgz.name, tgz.salary, tgz.seats_available
--- from
--- (
-
 select 
-mu.id
-,mu.name
-,mu.contact_number
-,ca.make
-,mp.is_pet
-,avg(ri.price_per_head) as salary
-,src.state_name
-,sum(ri.seats_available) as pplcount
-,count(ri.created_on) as days
+    mu.id
+    ,mu.name
+    ,mu.contact_number
+    ,ca.make
+    ,mp.is_pet
+    ,avg(ri.price_per_head) as salary
+    ,src.state_name
+    ,sum(ri.seats_available) as pplcount
+    ,count(ri.created_on) as days
 from
-memberuser mu
-,memberuser_car mc
-,car ca
-,memberuser_preference mp
-,ride ri
-,city src
-,city dst
+    memberuser mu
+    ,memberuser_car mc
+    ,car ca
+    ,memberuser_preference mp
+    ,ride ri
+    ,city src
+    ,city dst
 where 
-mu.id=mc.memberuser_id
-and mc.car_id=ca.id
-and mp.memberuser_id=mu.id
-and ri.memberuser_car_id=mc.id
-and ri.src_city_id=src.id
-and ri.dst_city_id=dst.id
-and ca.make <> ALL (select make from car where make like 'Opel' or make like 'Daewoo')
-and mp.is_pet <> 'N'
-and (src.state_name <> 'Zhytomyr Oblast' and src.state_name <> 'Vinnytsia Oblast')
-and (dst.state_name <> 'Zhytomyr Oblast' and dst.state_name <> 'Vinnytsia Oblast')
-and ri.created_on::text like '____-11-__'
-  
-  group by
-  mu.id
-  ,ca.make
-  ,mp.is_pet
-  ,src.state_name
-	having avg(ri.price_per_head) > ALL
-	(
-	select
-	avg(ri.price_per_head)
-	from
-	memberuser_car mc
-	,ride ri
-	,city src
-	,city dst
-	where
-	mc.id=ri.memberuser_car_id
-	and ri.src_city_id=src.id
-	and ri.dst_city_id=dst.id
-	and (src.state_name <> 'Zhytomyr Oblast' and src.state_name <> 'Vinnytsia Oblast')
-	and (dst.state_name <> 'Zhytomyr Oblast' and dst.state_name <> 'Vinnytsia Oblast')
-	group by src.state_name 
+    mu.id=mc.memberuser_id
+    and mc.car_id=ca.id
+    and mp.memberuser_id=mu.id
+    and ri.memberuser_car_id=mc.id
+    and ri.src_city_id=src.id
+    and ri.dst_city_id=dst.id
+    and ca.make <> ALL (select make from car where make like 'Opel' or make like 'Daewoo')
+    and mp.is_pet <> 'N'
+    and (src.state_name <> 'Zhytomyr Oblast' and src.state_name <> 'Vinnytsia Oblast')
+    and (dst.state_name <> 'Zhytomyr Oblast' and dst.state_name <> 'Vinnytsia Oblast')
+    and ri.created_on::text like '____-11-__'
+group by
+    mu.id
+    ,ca.make
+    ,mp.is_pet
+    ,src.state_name
+having avg(ri.price_per_head) > ALL
+    (
+	    select
+	        avg(ri.price_per_head)
+	    from
+	        memberuser_car mc
+	        ,ride ri
+	        ,city src
+	        ,city dst
+	    where
+	        mc.id=ri.memberuser_car_id
+	        and ri.src_city_id=src.id
+	        and ri.dst_city_id=dst.id
+	        and (src.state_name <> 'Zhytomyr Oblast' and src.state_name <> 'Vinnytsia Oblast')
+	        and (dst.state_name <> 'Zhytomyr Oblast' and dst.state_name <> 'Vinnytsia Oblast')
+	    group by src.state_name 
 	)
 ;
